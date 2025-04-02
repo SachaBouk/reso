ca5a785b120ddfa7c8659d99180fd4be357b1f5a
JackoLaPatate <martignatnoam@gmail.com>
2025-04-02 13:24:07 +0200

    ajout page de tout les utilisateurs

--- contacts/otherProfile.php
+++ contacts/otherProfile.php
@@ -1,1 +1,14 @@
 <?php
+$connexion = mysqli_connect("localhost:25566","root","lecacaestcuit", "reso");
+$user_id = $_GET["user"];
+
+$query = "SELECT * FROM users WHERE user_id = '$user_id'";
+$request = mysqli_query($connexion, $query);
+
+if ($user = mysqli_fetch_assoc($request)) {
+    echo "<br>Nom Public: " . htmlspecialchars($user["publicName"]);
+    echo "<br>Nom d'utilisateur: " . htmlspecialchars($user["username"]);
+    echo "<br>Date de création: " . htmlspecialchars($user["creationDate"]);
+    echo "<br>Followers: " . htmlspecialchars($user["followers"]);
+    echo "<br>Following: " . htmlspecialchars($user["following"]);
+}
