79488252bc68227dfb50d0bde13a6305e0c562e6
JackoLaPatate <martignatnoam@gmail.com>
2025-04-02 08:50:36 +0200

    prototype index.php

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Accueil</title>
 </head>
 <body>
-
+    <a href="authentification/register.php">Register</a>
+    <a href="authentification/login.php">Login</a>
+    <a href="contacts/profile.php">Profile</a>
+    <a href="messages/post.php">Post test</a>
+    <a href="authentification/logout.php">Logout</a>
 </body>
 </html>
