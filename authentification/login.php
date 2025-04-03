c927df53b5c93fcd7ca177b106611e2b067ad642
SachaBouk <sachaboukouya@gmail.com>
2025-04-03 10:02:41 +0200

    Decrease login form container height from 100vh to 90vh for improved layout

--- authentification/login.php
+++ authentification/login.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    height: 90vh;
 }
 
 .login-container {
